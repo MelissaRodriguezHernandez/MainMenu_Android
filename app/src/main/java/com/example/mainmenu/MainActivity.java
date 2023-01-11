ca6d8f94f523c8495f562126c542aeef0a215be8
MelissaRodriguezHernandez <mrodriguezhernandez@cifpfbmoll.eu>
2023-01-11 02:23:07 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mainmenu/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.mainmenu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView menuList = (ListView) findViewById(R.id.list);
+
+        String[] items = { getResources().getString(R.string.menu_item_play),
+                getResources().getString(R.string.menu_item_scores),
+                getResources().getString(R.string.menu_item_settings),
+                getResources().getString(R.string.menu_item_help) };
+
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, R.layout.list, items);
+
+        menuList.setAdapter(adapt);
+
+        menuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+
+                switch (position){
+                    case 0:
+                        Toast.makeText(MainActivity.this, "1", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Toast.makeText(MainActivity.this, "2", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Toast.makeText(MainActivity.this, "3", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        Toast.makeText(MainActivity.this, "4", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+}
