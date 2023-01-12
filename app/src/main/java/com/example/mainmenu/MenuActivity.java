4f02d4eba75362bc48e7730c744dcde3adc7a34f
MelissaRodriguezHernandez <mrodriguezhernandez@cifpfbmoll.eu>
2023-01-12 02:00:14 +0100

    commit1

--- app/src/main/java/com/example/mainmenu/MenuActivity.java
+++ app/src/main/java/com/example/mainmenu/MenuActivity.java
@@ -2,6 +2,7 @@ package com.example.mainmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,14 +10,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
 
         ListView menuList = (ListView) findViewById(R.id.list);
+        menuList.setClickable(true);
 
         String[] items = { getResources().getString(R.string.menu_item_play),
                 getResources().getString(R.string.menu_item_scores),
@@ -33,16 +35,17 @@ public class MainActivity extends AppCompatActivity {
 
                 switch (position){
                     case 0:
-                        Toast.makeText(MainActivity.this, "1", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MenuActivity.this, SelectorActivity.class);
+                        startActivity(intent);
                         break;
                     case 1:
-                        Toast.makeText(MainActivity.this, "2", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MenuActivity.this, "2", Toast.LENGTH_SHORT).show();
                         break;
                     case 2:
-                        Toast.makeText(MainActivity.this, "3", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MenuActivity.this, "3", Toast.LENGTH_SHORT).show();
                         break;
                     case 3:
-                        Toast.makeText(MainActivity.this, "4", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MenuActivity.this, "4", Toast.LENGTH_SHORT).show();
                         break;
                 }
             }
